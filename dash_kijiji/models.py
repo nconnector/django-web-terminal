6b24580ac0d8aea428ea25544bb577fe5a7f8ff6
Nick <kuznaik@gmail.com>
2019-10-19 01:28:52 -0500

    before activating models

--- dash_kijiji/models.py
+++ dash_kijiji/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+    unm = models.CharField(max_length=32)
+    pwd = models.CharField(max_length=16)
+
+
+class Case(models.Model):
+    user = models.ForeignKey(Client, on_delete=models.CASCADE)
+    sampleurl = models.CharField(max_length=200)
+    data = models.TextField(default='')
+
