9fbcba10594f0c372bb8839d2158fa6c3315f7d0
Nick <kuznaik@gmail.com>
2019-10-19 02:33:54 -0500

    mongoDB & djongo engine

--- dash_kijiji/models.py
+++ dash_kijiji/models.py
@@ -4,12 +4,28 @@ from django.db import models
 
 
 class Client(models.Model):
+    def __str__(self):
+        return self.unm
+
     unm = models.CharField(max_length=32)
     pwd = models.CharField(max_length=16)
 
 
 class Case(models.Model):
+    def __str__(self):
+        return self.data
+
     user = models.ForeignKey(Client, on_delete=models.CASCADE)
     sampleurl = models.CharField(max_length=200)
     data = models.TextField(default='')
 
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- djangodashboard/settings.py
+++ djangodashboard/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'dash_kijiji',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,10 +77,14 @@ WSGI_APPLICATION = 'djangodashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'HOST': f"mongodb://{config['MONGO']['USERNAME']}:{config['MONGO']['PASSWORD']}@{config['MONGO']['IP']}/{config['MONGO']['DATABASE']}",
+        'USER': config['MONGO']['USERNAME'],
+        'PASSWORD': config['MONGO']['PASSWORD'],
+        'NAME': config['MONGO']['DATABASE'],
     }
 }
 
