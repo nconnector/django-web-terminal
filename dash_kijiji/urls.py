b07c297cf414ce0b6e3f06c296ec4dd1c1e5cb57
Nick <kuznaik@gmail.com>
2019-10-21 04:08:18 -0500

    - Starting to add pages - Main view lists client list - Profile shown by username

--- dash_kijiji/urls.py
+++ dash_kijiji/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.Main.as_view(), name='main'),
     path('login/', views.Login.as_view(), name='login'),
-    path('profile/', views.Profile.as_view(), name='profile'),
+    path('profile/<username>/', views.Profile.as_view(), name='profile'),
     path('about/', views.About.as_view(), name='about'),
 
     path('flow/', views.Flow.as_view(), name='flow'),
--- dash_kijiji/views.py
+++ dash_kijiji/views.py
@@ -3,9 +3,10 @@ from django.views.generic.base import View
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 
+from .models import Client, Case
+
+
 # Create your views here.
-
-
 class Flow(View):
     def get(self, request, *args, **kwargs):
         workflow = [
@@ -25,9 +26,10 @@ class Flow(View):
             """VIEWS LAYOUT:\r\n
     [✔] Flow: debug view
     [✘] Login
-    [✘] Welcome: if logged_in
-    [✘] My Profile
-    [✘] About
+    [✘] Main page: list of users for admin or redirect to own Profile
+    [✘] Profile
+    [✔] About
+    [✘] Extendable Base Page for {% extends %}
     """,
 
         ]
@@ -43,18 +45,32 @@ class Login(View):
 
 class Main(View):
     def get(self, request, **kwargs):
-        response = HttpResponse(f'Main page')
+        """
+        List all the clients
+        todo: with their ads
+        """
+        client_list = [str(x) for x in Client.objects.all()]
+        case_list = []
+
+        # context = {
+        #     'Clients': client_list,
+        #     'Cases': case_list,
+        # }
+        # return render(request, "clentelle.html", context)
+
+        response = HttpResponse(f'<pre><h2>Welcome!</h2><h3>Client List</h3>{"<br>".join(client_list)}</pre>')
         return response
 
 
 class Profile(View):
-    def get(self, request, **kwargs):
-        response = HttpResponse(f'My Profile page')
+    def get(self, request, username, **kwargs):
+        admin = True
+        if admin:
+            response = HttpResponse(f'Profile page for <b>{username}</b>')
+        else:
+            response = None  # todo: redirect to own profile
         return response
 
 
-class About(View):
-    def get(self, request, **kwargs):
-        response = HttpResponse(f'About page')
-        return response
-
+class About(TemplateView):
+    template_name = 'dash_kijiji/about.html'
