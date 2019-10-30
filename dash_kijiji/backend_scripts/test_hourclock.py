0fe1564f66312798b90b1d66a0e68ee5487a111e
Nick <kuznaik@gmail.com>
2019-10-30 15:54:58 -0500

    - long running subprocess test

--- dash_kijiji/backend_scripts/test_hourclock.py
+++ dash_kijiji/backend_scripts/test_hourclock.py
@@ -2,8 +2,7 @@ from time import sleep, gmtime, strftime
 import sys
 
 
-for i in range(5):
+while True:
     time_cur = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     print(time_cur)
-    sleep(2)
-# sleep(60*60)
+    sleep(10)
