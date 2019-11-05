294e56ab7ec51e109c47f61fb39f512bae269558
Nick <kuznaik@gmail.com>
2019-11-05 02:09:28 -0600

    - bootstrap

--- dash_kijiji/static/dash_kijiji/js/case_action.js
+++
@@ -1,9 +0,0 @@
-$(".action_form").on("submit", function(event){
-          event.preventDefault();
-          console.log("form submitted!");
-          $.ajax({
-            type : "POST",
-            data : $(this).serializeArray(),
-            success : function(json) {console.log(json)}, // log the returned json to the console
-          });
-        });
